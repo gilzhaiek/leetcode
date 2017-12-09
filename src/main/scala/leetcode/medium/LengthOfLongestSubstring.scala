599c1bb0a492358962796809676ca3120bd28858
Gil Zhaiek <gilzhaiek@hotmail.com>
2017-12-08 21:36:49 -0800

    Added Length of the Longest Substring https://leetcode.com/problems/longest-substring-without-repeating-characters/description/

--- src/main/scala/leetcode/medium/LengthOfLongestSubstring.scala
+++ src/main/scala/leetcode/medium/LengthOfLongestSubstring.scala
@@ -2,17 +2,55 @@ package leetcode.medium
 
 /**
   * https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
-  * 
+  *
   * Given a string, find the length of the longest substring without repeating characters.
   *
-  * Given "abcabcbb", the answer is "abc", which the length is 3.
+  * abbc b bca cab abc bc b b
+  * ab bc b bca cab abc bc b b
+  * Given "abbcabcbb", the answer is "bca", which the length is 3.
   * Given "bbbbb", the answer is "b", with the length of 1.
   * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring,
   *
   * Created by gilzhaiek on 2017-12-07.
   */
 class LengthOfLongestSubstring {
-  def lengthOfLongestSubstring(s: String): Int = {
-    0
+  def lengthOfLongestSubstringONSqr(s: String, offset: Int): Int = {
+    val nextOffset = offset + 1
+    if (nextOffset >= s.length()) {
+      return s.length
+    }
+    val chr: Char = s.charAt(offset)
+    val nextRecurring = s.indexOf(chr, nextOffset)
+    if (nextRecurring >= 0) {
+      lengthOfLongestSubstringONSqr(s.substring(0, nextRecurring), nextOffset)
+    } else {
+      lengthOfLongestSubstringONSqr(s, nextOffset)
+    }
+  }
+
+  def lengthOfLongestSubstringONSqr(s: String): Int = {
+    if (s.length == 0) {
+      return 0
+    }
+    s.indices.map(index => lengthOfLongestSubstringONSqr(s.substring(index), 0)).max
+  }
+
+  def lengthOfLongestSubstringON(s: String): Int = {
+    val toIndex: Int = s.length
+    val set = scala.collection.mutable.HashSet.empty[Char]
+    var ans, i, j: Int = 0
+
+    while (i < toIndex && j < toIndex) {
+      if (!set.contains(s.charAt(j))) {
+        set.add(s.charAt(j))
+        j += 1
+        ans = Math.max(ans, j - i)
+      }
+      else {
+        set.remove(s.charAt(i))
+        i += 1
+      }
+    }
+    ans
   }
 }
--- src/test/scala/leetcode/medium/LengthOfLongestSubstringSpec.scala
+++ src/test/scala/leetcode/medium/LengthOfLongestSubstringSpec.scala
@@ -14,7 +14,13 @@ class LengthOfLongestSubstringSpec extends UnitSpec {
   * Explanation: wke has length of 3
    */
   "LengthOfLongestSubstring" should " find the length of the longest substring without repeating characters" in {
-    var str:String = "pwwkew"
-    obj.lengthOfLongestSubstring(str) should equal(3)
+    obj.lengthOfLongestSubstringONSqr("pwwkew") should equal(3)
+    obj.lengthOfLongestSubstringON("pwwkew") should equal(3)
+
+    obj.lengthOfLongestSubstringONSqr("abcabcbb") should equal(3)
+    obj.lengthOfLongestSubstringON("pwwkew") should equal(3)
+
+    obj.lengthOfLongestSubstringONSqr("abbcabcbb") should equal(3)
+    obj.lengthOfLongestSubstringON("pwwkew") should equal(3)
   }
 }
